6222978a3c41c69b862159b2de1f2d7966951b61
Jessica276 <rojotianarahajaharivony@gmail.com>
2024-12-19 23:43:47 +0300

    sms-spam-classification

--- preprocessing.py
+++ preprocessing.py
@@ -1,7 +1,20 @@
+import gdown
+import chardet
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
 def preprocessing():
+    url = "https://drive.google.com/uc?id=1gg-OD4pQeyShfUt4w7QqxYQDOeS3trF0"
+    output = "spam.csv"
+    gdown.download(url, output, quiet=False)
+
+    with open(output, "rb") as file:
+        result = chardet.detect(file.read())
+    encoding = result["encoding"]
+
+    # Read file
+    data = pd.read_csv(output, encoding=encoding)
+
     # Drop unnecessary columns
     data = data.drop(["Unnamed: 2", "Unnamed: 3", "Unnamed: 4"], axis=1)
     data.columns = ["label", "text"]
